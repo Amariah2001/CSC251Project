90288f05ec44945cdf056af0ab13069821db2c57
Amariah Robinson <arobin95@email.cpcc.edu>
2024-02-12 21:41:30 -0500

    Update Policy.java
    Changed some code for Policy Class

--- Policy.java
+++ Policy.java
@@ -13,14 +13,14 @@ public class Policy {
     private String policyLastName; // used to get policyholder's last name
     private int policyAge; // used to get policyholder's age
     private String policyStatus; // used to get policyholder's smoking status
-    private int policyHeight; // used to get policyholder's height in inches
+    private double policyHeight; // used to get policyholder's height in inches
     private double policyWeight; // used to get policyholder's weight in pounds
     private double policyBMI; // used to get the policyholder's BMI
-    private double BASE_FEE = 600; // base fee for the insurance policy
+    private double BASE_FEE = 600.00; // base fee for the insurance policy
     private double totalPrice; // used to get the total price of the insurance policy
     private double additionalFee; // used to calculate the additional fee of the insurance policy, if needed
-    private double ADD_FEE75 = 75; // used if the policyholder is > 50 years old
-    private double ADD_FEE100 = 100; // used when the policyholder is a smoker
+    private double ADD_FEE75 = 75.00; // used if the policyholder is > 50 years old
+    private double ADD_FEE100 = 100.00; // used when the policyholder is a smoker
 
     Policy() { // no arg constructor contains default values if setters and getters are not made correctly!
         policyNum = 0;
@@ -93,11 +93,11 @@ public class Policy {
         return policyStatus;
     }
 
-    public void setPolicyHeight(int polyHeight) {
+    public void setPolicyHeight(double polyHeight) {
         policyHeight = polyHeight;
     }
 
-    public int getPolicyHeight() {
+    public double getPolicyHeight() {
         return policyHeight;
     }
 
@@ -113,7 +113,7 @@ public class Policy {
         policyBMI = BMI;
     }
 
-    public double getBMI(double policyWeight, int policyHeight) { // method used to calculate and return the BMI of the policyholder
+    public double getBMI(double policyWeight, double policyHeight) { // method used to calculate and return the BMI of the policyholder
         policyBMI = (policyWeight * 703) / (policyHeight * policyHeight);
         return policyBMI;
     }
@@ -126,10 +126,10 @@ public class Policy {
         if (policyAge > 50) {
             totalPrice = BASE_FEE + ADD_FEE75;
         }
-        else if (policyStatus == "smoker") {
+        if (policyStatus == "smoker" || policyStatus == "Smoker") {
             totalPrice = BASE_FEE + ADD_FEE100;
         }
-        else if (policyBMI > 35) {
+        if (policyBMI > 35) {
             additionalFee = (policyBMI - 35) * 20; 
             totalPrice = BASE_FEE + additionalFee;
         }
